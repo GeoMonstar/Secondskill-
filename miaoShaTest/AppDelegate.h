19f510fa80ff4371ebfc6192ed4a5984da8b4723
Monstar <Monstar@monstarmacbook-pro.local>
2017-09-04 16:13:17 +0800

    Initial Commit

---
+++ miaoShaTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  miaoShaTest
+//
+//  Created by Monstar on 2017/9/4.
+//  Copyright © 2017年 Monstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ miaoShaTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  miaoShaTest
+//
+//  Created by Monstar on 2017/9/4.
+//  Copyright © 2017年 Monstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
